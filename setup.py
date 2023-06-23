a5f165e0cca79e40d040f53372ae9f05d892b567
novafacing <rowan.hart@intel.com>
2023-06-23 11:52:45 -0700

    Use old angr

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ import os
 from distutils.core import setup
 
 setup(
-    name='fuzzer', version='1.1', description="Python wrapper for multiarch AFL",
-    packages=['fuzzer', 'fuzzer.extensions'],
-    data_files = [ ("bin", (os.path.join("bin", "create_dict.py"),)) ],
-    scripts = [ 'shellphuzz' ],
-    install_requires=['angr', 'shellphish-qemu', 'shellphish-afl', 'tqdm']
+    name="fuzzer",
+    version="1.1",
+    description="Python wrapper for multiarch AFL",
+    packages=["fuzzer", "fuzzer.extensions"],
+    data_files=[("bin", (os.path.join("bin", "create_dict.py"),))],
+    scripts=["shellphuzz"],
+    install_requires=["angr==8.20.7.27", "shellphish-qemu", "shellphish-afl", "tqdm"],
 )
